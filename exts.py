135ad2fdfe3b7c6522049287e94bfdeed0ab7c5e
zengke123 <249626102@qq.com>
2018-07-11 01:53:59 +0800

    调整例检配置页面

--- exts.py
+++ exts.py
@@ -9,3 +9,30 @@ def create_app():
     app.config.from_object(config)
     db.init_app(app)
     return app
+
+
+def auto_check(lj_type, name):
+    import pexpect
+    username = "root"
+    password = '123456'
+    cmd = 'zj zjlj lj:type={},name={}'.format(lj_type, name)
+    child = pexpect.spawn('telnet 0 47303', timeout=300)
+    child.expect('login:')
+    child.sendline(username)
+    child.expect('password:')
+    child.sendline(password)
+    child.expect('root > ')
+    child.sendline(cmd)
+    child.expect('{}root >')
+    result = child.before
+    # RETN=0000, DESC=OK
+    # RETN=0006
+    if 'RETN=0000' in result:
+        flag = True
+    else:
+        flag = False
+    # print(child.before)
+    child.sendline('quit')
+    # print(child.before)
+    child.close()
+    return flag
--- webapp.py
+++ webapp.py
@@ -1,5 +1,4 @@
-from collections import namedtuple
-from exts import create_app, db
+from exts import create_app, db, auto_check
 from models import User, Host
 from sqlalchemy import distinct
 from decorators import login_required
@@ -30,10 +29,12 @@ def login():
             error = "账号{}不存在！".format(username)
             return render_template('login.html', error=error)
 
-@app.route('/logout/')
+
+@app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('login'))
+
 
 @app.route('/')
 @login_required
@@ -41,22 +42,41 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/user/')
+@app.route('/user', methods=['GET', 'POST'])
 def user():
-    return render_template('user.html')
+    error = None
+    if request.method == 'GET':
+        return render_template('user.html', error=error)
+    else:
+        username = session.get('username')
+        oldpassword = request.form.get('oldpassword')
+        newpassword1 = request.form.get('newpassword1')
+        newpassword2 = request.form.get('newpassword2')
+        result = User.query.filter(User.username == username).first()
+        if result:
+            if oldpassword == result.password:
+                if newpassword1 == newpassword2:
+                    result.password = newpassword2
+                    db.session.commit()
+                    error = "密码修改成功！"
+                else:
+                    error = "两次输入密码不一致！"
+            else:
+                error = "原密码输入错误！"
+    return render_template('user.html', error=error)
 
 
-@app.route('/check/')
+@app.route('/check')
 @login_required
 def check():
     data = []
     cluste_type = db.session.query(distinct(Host.type)).all()
     types = [x[0] for x in cluste_type]
-    for type in types:
-        cluste_temps = Host.query.filter(Host.type == type).all()
-        clusters = [x.cluster for x in cluste_temps]
-        data.append((type,clusters))
-    print(data)
+    for c_type in types:
+        # cluste_temps = Host.query.filter(Host.type == type).distinct().all()
+        cluste_temps = db.session.query(distinct(Host.cluster)).filter(Host.type == c_type).all()
+        clusters = [x[0] for x in cluste_temps]
+        data.append((c_type, clusters))
     return render_template('check.html',data=data)
 
 
@@ -71,30 +91,57 @@ def check_host():
 
 @app.route('/check/lj/')
 @login_required
-def auto_check():
-    name = request.args.get('name')
-    return "调用自动例检接口，参数值{}".format(name)
+def lj():
+    clustename = request.args.get('clustename', None)
+    hostname = request.args.get('hostname', None)
+    if clustename:
+        result = auto_check(lj_type='jq', name=clustename)
+        # return "调用集群自动例检接口，参数值{}".format(clustename)
+    else:
+        result = auto_check(lj_type='zj', name=hostname)
+        # return "调用主机自动例检接口，参数值{}".format(hostname)
+    return result
 
-
-@app.route('/check/setting/')
+@app.route('/check/setting')
 @login_required
 def check_setting():
-    return render_template('check_setting.html')
 
+    # datas = db.session.query(Host).all()
+    class_type = ['One','Two','Three','Four','Five','Six', 'Seven','Eight','Nine','Ten','Eleven','Twelve']
+    datas = []
+    cluste_type = db.session.query(distinct(Host.type)).all()
+    types = [x[0] for x in cluste_type]
+    nums = len(types)
+    for i, c_type in enumerate(types):
+        cluste_temps = db.session.query(Host).filter(Host.type == c_type).all()
+        print(cluste_temps)
+        # clusters = [x[0] for x in cluste_temps]
+        datas.append((c_type, cluste_temps,class_type[i]))
+    return render_template('check_setting.html',datas=datas)
+    # print(datas)
+    # return "测试"
 
-@app.route('/check/setting/add/')
+@app.route('/check/setting/add/', methods=['GET', 'POST'])
 @login_required
 def check_add():
-    return render_template('check_add.html')
+    type = request.form.get('type')
+    cluster = request.form.get('cluster')
+    hosts_temp = request.form.get('hostname')
+    hosts = hosts_temp.split('|')
+    for host in hosts:
+        add_host = Host(type=type, cluster=cluster, hostname=host)
+        db.session.add(add_host)
+    db.session.commit()
+    # return render_template('check_add.html')
+    return "添加成功"
 
-
-@app.route('/check/setting/del/')
+@app.route('/check/setting/del')
 @login_required
 def check_del():
     return render_template('check_del.html')
 
 
-@app.route('/query/')
+@app.route('/query')
 @login_required
 def query():
     return render_template('query.html')
